a1f0ca91afa2c6b7a04cd9f9f2ffbba42cafa162
MakinoRuki <muye67@gmail.com>
2019-06-18 01:51:40 -0700

    Update 2d-fft-cuda.cpp

--- toyexamples/FFT/2d-fft-cuda.cpp
+++ toyexamples/FFT/2d-fft-cuda.cpp
@@ -1,29 +1,42 @@
 #include "2d-fft-cuda.hpp"
 #include "fft-cuda.cuh"
 
-void fft_2d(vector<pair<float, float> >& a, int n, int m, int threads) {
+void test_fft(vector<pair<float, float> >& a, int n, int threads, bool isrev) {
+    float2 b[n];
+    for (int i = 0; i < n; ++i) {
+        b[i].x = a[i].first;
+        b[i].y = a[i].second;
+    }
+    fft(b, n, threads, isrev);
+    for (int i = 0; i < n; ++i) {
+        a[i].first = b[i].x;
+        a[i].second = b[i].y;
+    }
+}
+
+void fft_1d(vector<pair<float, float> >& a, int n, int m, int threads, bool isrev, bool byrow) {
     for (int i = 0; i < n; ++i) {
         float2 b[m];
         for (int j = 0; j < m; ++j) {
-            b[j].x = a[i * m + j].first;
-            b[j].y = a[i * m + j].second;
+            int k = (byrow ? i * m + j : j * n + i);
+            b[j].x = a[k].first;
+            b[j].y = a[k].second;
         }
-        fft(b, m, threads);
+        fft(b, m, threads, isrev);
         for (int j = 0; j < m; ++j) {
-            a[i * m + j].first = b[j].x;
-            a[i * m + j].second = b[j].y;
+            int k = (byrow ? i * m + j : j * n + i);
+            a[k].first = b[j].x;
+            a[k].second = b[j].y;
         }
     }
-    for (int j = 0; j < m; ++j) {
-        float2 b[n];
-        for (int i = 0; i < n; ++i) {
-            b[i].x = a[i * m + j].first;
-            b[i].y = a[i * m + j].second;
-        }
-        fft(b, n, threads);
-        for (int i = 0; i < n; ++i) {
-            a[i * m + j].first = b[i].x;
-            a[i * m + j].second = b[i].y;
-        }
+}
+
+void fft_2d(vector<pair<float, float> >& a, int n, int m, int threads, bool isrev) {
+    if (!isrev) {
+        fft_1d(a, n, m, threads, isrev, true);
+        fft_1d(a, m, n, threads, isrev, false);
+    } else {
+        fft_1d(a, m, n, threads, isrev, false);
+        fft_1d(a, n, m, threads, isrev, true);
     }
 }

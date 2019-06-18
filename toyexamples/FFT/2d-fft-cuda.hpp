266135822cebf702ab496aafcad1e2c208f8b7f9
MakinoRuki <muye67@gmail.com>
2019-06-18 01:52:24 -0700

    Update 2d-fft-cuda.hpp

--- toyexamples/FFT/2d-fft-cuda.hpp
+++ toyexamples/FFT/2d-fft-cuda.hpp
@@ -4,18 +4,19 @@
 #include <sys/time.h>
 #include <unistd.h>
 #include <vector>
-//#include "fft-cuda.cuh"
+#include <opencv2/opencv.hpp>
 // Seems cuda.h can not in .hpp but should in .cuh
 //#include <cuda.h>
 //#include <cuda_runtime.h>
 using namespace std;
-//#include <helper_cuda.h>
 
 
 #define RANDOM(x) (rand() % x)
 
-#define MAX 100000
+//#define MAX 100000
 
 #define BLOCKSIZE 16
 
-void fft_2d(vector<pair<float, float> >& a, int n, int m, int threads);
+void test_fft(vector<pair<float, float> >& a, int n, int threads, bool isrev);
+
+void fft_2d(vector<pair<float, float> >& a, int n, int m, int threads, bool isrev);
